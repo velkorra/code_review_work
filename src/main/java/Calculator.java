5d0212d9b3f810a8de4127247c2dd765f98bf849
velkorra <anton.s.vertunov@gmail.com>
2024-11-29 14:20:01 +0300

    feat: implement calculator solving

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,21 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        return a / b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver(int x, int y) {
+        return add(x, y) * times(x, y) / (div(dif(x, y), add(x * x, 2 * y)));
     }
 }
